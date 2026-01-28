022a3708a732b35290a8cea15b36a7890184055c
Nit <nitithon.taksaphan@gmail.com>
2026-01-28 10:46:33 +0700

    feat: implement moveZeroes and romanToInt functions with optimized logic

--- th/ex-1.js
+++ th/ex-1.js
@@ -1,5 +1,24 @@
 const moveZeroes = function (nums) {
   //Start coding here
+  
+  // filter, spread
+  let notZero = nums.filter(e => e !== 0)
+  let zero = nums.filter(e => e === 0)
+  let ans = [...notZero, ...zero]
+  return ans
+
+  //two pointer
+  let left = 0
+  nums
+  for (let right = 0; right < nums.length; right++) {
+    if (nums[right] !== 0) {
+      let temp = nums[right]
+      nums[right] = nums[left]
+      nums[left] = temp
+      left++
+    } 
+  }
+  return nums
 };
 
 const result1 = moveZeroes([0, 1, 0, 3, 12]);
--- th/ex-2.js
+++ th/ex-2.js
@@ -1,5 +1,28 @@
+
+const romanMap = {
+  'I': 1,
+  'V': 5,
+  'X': 10,
+  'L': 50,
+  'C': 100,
+  'D': 500,
+  'M': 1000,
+};
+
 const romanToInt = function (s) {
   //Start coding here
+  let nums = s.split('').map(e => romanMap[e])
+  let total = 0
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] >= (nums[i + 1] ?? 0)) {
+      total += nums[i]
+    } else {
+      let temp = nums[i + 1] - nums[i]
+      total += temp
+      i++
+    }
+  }
+  return total
 };
 
 const result1 = romanToInt("III"); // 3
@@ -10,3 +33,12 @@ console.log(result2);
 
 const result3 = romanToInt("MCMXCIV"); //1994
 console.log(result3);
+
+const result4 = romanToInt("MCMXCVII"); 
+console.log(result4);
+
+const result5 = romanToInt("MCVI");
+console.log(result5)
+
+const result6 = romanToInt("CXCV"); 
+console.log(result6);
